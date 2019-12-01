a27f3fbdb8e75be3d063f5fcc0001775ef182f66
Matt Cuneo <m@jcuneo.com>
2019-12-02 10:46:37 +1100

    wrong way

--- scripts/lg.sh
+++ scripts/lg.sh
@@ -3,9 +3,9 @@ BS_PATH=/usr/local/bootstrap
 source "$BS_PATH/.env"
 
 if [ -z "${DEBUG:-}" ]; then
-  exec 3>&1
-else
   exec 3>&1 &>/dev/null
+else
+  exec 3>&1
 fi
 lg() {
   echo ">> [$(date '+%Y-%m-%d %H:%M:%S')]: $@" >&3
