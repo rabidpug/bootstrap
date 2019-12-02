f5df7559aa6b810de3724cf27f896de9a1fc8de6
Matt <m@jcuneo.com>
2019-12-02 15:49:29 +1100

    Make cronjobs executable and ordered

--- scripts/cronjobs.sh
+++ scripts/cronjobs.sh
@@ -9,5 +9,6 @@ source "$BS_PATH/scripts/lg.sh"
 lg '##CRONJOBS##'
 find "$BS_PATH/cronjobs" -type f | while read job; do
   lg "Create/update symlink for $(basename $job)"
+  chmod +x "$job"
   ln -sf "$job" /etc/cron.daily
 done
