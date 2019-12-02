f7f01b29a0ab58d25302ecafbd59205db37e81f0
Matt <m@jcuneo.com>
2019-12-03 07:30:11 +1100

    fixed cron path

--- scripts/6-cronjobs.sh
+++ scripts/6-cronjobs.sh
@@ -7,7 +7,7 @@ source "$BS_PATH/.env"
 source "$BS_PATH/scripts/lg.sh"
 
 lg '## BEGINNING CRONJOBS ##'
-find "$BS_PATH/cronjobs" -type d | while read folder; do
+find "$BS_PATH/cron" -type d | while read folder; do
   interval=$(basename "$folder")
   find "$folder" -type f | while read job; do
     lg "Create/update symlink for $(basename $job) in cron.$interval"
