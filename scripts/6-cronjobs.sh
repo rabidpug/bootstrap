aef650d2cf9c65cc7eabf93e082043eb9a5f1f75
Matt <m@jcuneo.com>
2019-12-03 07:53:36 +1100

    Added log rotation

--- scripts/6-cronjobs.sh
+++ scripts/6-cronjobs.sh
@@ -7,10 +7,24 @@ source "$BS_PATH/.env"
 source "$BS_PATH/scripts/lg.sh"
 
 lg '## BEGINNING CRONJOBS ##'
+
+lg 'Configure log rotation'
+cat <<EOT >>/etc/logrotate.d/bs
+/var/log/bs.log {
+daily
+copytruncate
+missingok
+dateext
+rotate 7
+compress
+}
+
+EOT
+
 find "$BS_PATH/cron" -type d | while read folder; do
   interval=$(basename "$folder")
   find "$folder" -type f | while read job; do
-    lg "Create/update symlink for $(basename $job) in cron.$interval"
+    lg "Create symlink for $(basename $job) in cron.$interval"
     chmod +x "$job"
     ln -sf "$job" "/etc/cron.$interval"
   done
